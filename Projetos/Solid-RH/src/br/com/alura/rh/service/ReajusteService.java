45bcdf79d7cde8d791723fb103851ec05f41e2e8
ThiagoBuen <thiagobueno@gea.inatel.br>
2022-01-22 12:09:03 -0300

    22/01 - Solid start

---
+++ Projetos/Solid-RH/src/br/com/alura/rh/service/ReajusteService.java
@@ -0,0 +1,30 @@
+package br.com.alura.rh.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+public class ReajusteService {
+
+		private List<ValidacaoReajuste> validacoes;
+		
+		public ReajusteService(List<ValidacaoReajuste> validacoes) {
+			this.validacoes = validacoes;
+		}
+		
+		public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
+			
+			
+			this.validacoes.forEach(v -> v.validar(funcionario, aumento));
+			
+						
+			BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
+			funcionario.atualizarSalario(salarioReajustado);
+						
+		}
+}
