36195b2041df251c7ef4fa7adcd913705e6900ae
ThiagoBuen <thiagobueno@gea.inatel.br>
2022-03-31 21:08:39 -0300

    31/03 - Spring JPA

--- Projetos/Spring MVC/mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ Projetos/Spring MVC/mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,31 +1,27 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
-
-		@PersistenceContext
-		private EntityManager entityManager;
 		
+		@Autowired
+		private PedidoRepository pedidoRepository;
+
 		@GetMapping("/home")
 		public String home(Model model) {
 			
-			Query query = entityManager.createQuery("select p from Pedido p", Pedido.class);
-			List<Pedido> pedidos = query.getResultList();
-			
+			List<Pedido> pedidos = pedidoRepository.findAll();
 			model.addAttribute("pedidos", pedidos);	
 			return "home";
 		}
+	
 }
---
+++ Projetos/Spring MVC/mudi/src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -0,0 +1,15 @@
+package br.com.alura.mvc.mudi.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+		
+	public List<Pedido> findAll();
+	
+}
