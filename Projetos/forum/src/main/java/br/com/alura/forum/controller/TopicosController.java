6824fcfd5f02ee6163e3dd297c66bdd92b4f5e22
ThiagoBuen <thiagobueno@gea.inatel.br>
2022-04-27 21:04:13 -0300

    27/03 - @PathVariable, @PutMapping, @Transactional, error treatment

--- Projetos/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ Projetos/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,19 +3,26 @@ package br.com.alura.forum.controller;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
@@ -42,6 +49,7 @@ public class TopicosController {
 	}
 	
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		
 		Topico topico = form.convert(cursoRepository);
@@ -50,4 +58,52 @@ public class TopicosController {
 		URI uri = uriBuilder.path("/topics/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));	
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id) {
+		Optional<Topico> topico = topicRepository.findById(id);
+		
+		if (topico.isPresent()) {
+			return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));
+		}
+	    
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
+		
+		Optional<Topico> optional = topicRepository.findById(id);
+		
+		if (optional.isPresent()) {
+			
+			Topico topico = form.atualizar(id, topicRepository);
+			
+			return ResponseEntity.ok(new TopicoDto(optional.get()));
+		}
+	    
+		return ResponseEntity.notFound().build();
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id){
+		
+		Optional<Topico> optional = topicRepository.findById(id);
+		
+		if (optional.isPresent()) {
+			
+			topicRepository.deleteById(id);
+			
+			return ResponseEntity.ok().build();
+		}
+	    
+		return ResponseEntity.notFound().build();
+		
+	}
+	
+	
+	
 }
---
+++ Projetos/forum/src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -0,0 +1,45 @@
+package br.com.alura.forum.controller.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicRepository;
+
+public class AtualizacaoTopicoForm {
+
+	@NotNull @NotEmpty @Length(min = 5)
+	private String titulo;
+	
+	@NotNull @NotEmpty @Length(min = 10)
+	private String mensagem;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public Topico atualizar(Long id, TopicRepository topicRepository) {
+		
+		Topico topico = topicRepository.getById(id);
+		topico.setTitulo(titulo);
+		topico.setMensagem(mensagem);
+		
+		return topico;
+	}
+	
+	
+}
